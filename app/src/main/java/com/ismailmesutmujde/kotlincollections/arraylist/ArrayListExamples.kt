f2f219aced6686034d508b465495917c5b4f74e0
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-17 15:41:12 +0300

    updated: arraylist - adding data and accessing data

--- app/src/main/java/com/ismailmesutmujde/kotlincollections/arraylist/ArrayListExamples.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincollections/arraylist/ArrayListExamples.kt
@@ -4,4 +4,29 @@ fun main() {
 
     val list = ArrayList<String>()
     val list2 : ArrayList<Int> = ArrayList<Int>()
+
+    val fruits = ArrayList<String>()
+    fruits.add("Strawberry")
+    fruits.add("Banana")
+    fruits.add("Apple")
+    fruits.add("Kiwi")
+    fruits.add("Cherry")
+
+    println(fruits.toString())
+
+    val str = fruits.get(2)
+    println(str)
+
+    val str1 = fruits[4]
+    println(str1)
+
+    fruits.add("Tangerine")
+    println(fruits.toString())
+
+    fruits[2] = "Pineapple"
+    println(fruits.toString())
+
+    fruits.add(3,"Orange")
+    println(fruits.toString())
+
 }
